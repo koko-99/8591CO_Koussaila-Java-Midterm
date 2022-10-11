b70f447bad2b59ed717dc361f0d0099932f5b1a9
koko-99 <okousseila@gmail.com>
2022-10-10 21:38:57 -0600

    10/10/22 Package Anagram is done

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,5 +1,7 @@
 package string_problems;
 
+import java.util.Arrays;
+
 public class Anagram {
 
     /** INSTRUCTIONS
@@ -15,4 +17,39 @@ public class Anagram {
 
     // Implement Here
 
-}
+
+        /**
+         Use this class to unit test all of the classes contained within this package
+         */
+
+
+        static boolean areAnagram(char[]str1, char[]str2){
+            int n1=str1.length;
+            int n2=str2.length;
+
+            if(n1 != n2)
+                return false;
+
+            Arrays.sort(str1);
+            Arrays.sort(str2);
+            for(int i=0; i<n1 ; i++)
+                if (str1[i] != str2[i])
+                    return false;
+
+            return true;
+
+        }
+        public static void main(String[] args) {
+            char str1[]={'a','r','m','y'};
+            char str2[]={'m','a','r','y'};
+
+            if(areAnagram(str1, str2))
+                System.out.println("the two Strings are anagram");
+            else
+                System.out.println("the two Strings are not anagram");
+
+
+        }
+    }
+
+

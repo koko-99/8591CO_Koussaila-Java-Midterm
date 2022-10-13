9eddef61047cfa299019b5fd2367ec3e25b2374e
koko-99 <okousseila@gmail.com>
2022-10-13 16:01:53 -0600

    Package LowestNumber is completed

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -1,17 +1,31 @@
 package math_problems;
 
+import java.util.Arrays;
+
 public class LowestNumber {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to find the lowest number from this array.
      */
 
     public static void main(String[] args) {
 
-        int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
-                67, 5, 679, 54, 32, 65};
+        int[] array = new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
+                67,5, 679, 54, 32, 65};
+        smallestNumber(array);
 
 
     }
 
-}
+
+        public static void smallestNumber(int[] array){
+
+            Arrays.sort(array);
+            System.out.println(array[0]);
+    }
+
+
+    }
+
+

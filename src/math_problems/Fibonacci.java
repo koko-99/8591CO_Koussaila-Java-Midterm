091cc9f3860d6e72df049ca1425464998028421c
koko-99 <okousseila@gmail.com>
2022-10-13 15:52:38 -0600

    Fibonacci package is done

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -9,6 +9,16 @@ public class Fibonacci {
      **/
 
     public static void main(String[] args) {
+        int n1=0;
+        int n2=1;
+
+        for(int i=2; i<=40;i++){
+            int sum=n1+n2;
+            System.out.println(" "+ sum);
+            n1=n2;
+            n2=sum;
+
+        }
 
     }
 }

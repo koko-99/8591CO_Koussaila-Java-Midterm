63837d67c3c8c72a70e8e50db46f5352e62d6c0c
koko-99 <okousseila@gmail.com>
2022-10-13 16:06:03 -0600

    PrimeNumber package is completed

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -11,7 +11,22 @@ public class PrimeNumber {
 
 
     public static void main(String[] args) {
+        for (int i = 2; i <= 1000000; i++) {
+            if (isPrime(i)) {
+                System.out.print(i);
+            } }
 
     }
+    public static boolean isPrime(int num) {
+        if (num == 2 || num == 3) {
+            return true; }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false; }
+        for (int i = 3; i < Math.sqrt(num); i += 2) {
+            if (num % i == 0 || num % Math.sqrt(num) == 0) {
+                return false; } }
+        return true; }
+
+
 
 }

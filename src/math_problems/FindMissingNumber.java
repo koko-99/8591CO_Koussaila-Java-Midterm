cdf75837b664b2a2ff69581f2d4c164d52e56a97
koko-99 <okousseila@gmail.com>
2022-10-13 16:00:27 -0600

    Package FindMissingNumber is completed

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -9,5 +9,19 @@ public class FindMissingNumber {
     public static void main(String[] args) {
         int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
 
+        int missingNumber = findMissingNumber(array);
+
+        System.out.println(missingNumber);
+    }
+    public static int findMissingNumber(int array[]){
+        int i;
+        int n=10;
+        int totalSum= n*(n+1)/2;
+        int arraySum=0;
+        for(i=0;i<array.length;i++){
+            arraySum=arraySum +array[i];
+        }
+        int missingNumber= totalSum-arraySum;
+        return missingNumber;
     }
 }

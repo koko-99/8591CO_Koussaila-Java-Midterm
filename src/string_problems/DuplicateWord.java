f14483cac4b82f4ce0df048c0defb16199cbb6a2
koko-99 <okousseila@gmail.com>
2022-10-13 16:07:56 -0600

    10/13/22 DuplicateWord package math_Problems is completed

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -1,5 +1,10 @@
 package string_problems;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class DuplicateWord {
 
     /** INSTRUCTIONS
@@ -11,7 +16,41 @@ public class DuplicateWord {
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
         // Implement here
+        String wordWithoutDot= st.replace(".", "");
+        String[] words = wordWithoutDot.split(" ");
+        Map<String,Integer> wordCount= new HashMap<>();
+        for (String word: words) {
+        Integer count= wordCount.get(word);
+        if (count==null){
+            count=0;
+        }
+        wordCount.put(word, count + 1);
+        }
+        Set<Map.Entry<String, Integer>> entrySet= wordCount.entrySet();
+        for (Map.Entry<String, Integer> entry: entrySet){
+            if (entry.getValue()>1){
+            System.out.println(entry.getKey() +":" + entry.getValue());
+        }}
+
+        calculateLength(st);
+
 
     }
+   static void calculateLength(String words ){
+       String wordWithoutDot= words.replace(".", "");
+       String wordWithoutDotAndSpace= words.replace(" ", "");
+       String[] arrayOfWords = wordWithoutDot.split(" ");
 
-}
+       int numOfWords= arrayOfWords.length;
+       int numOfChar= wordWithoutDotAndSpace.length();
+
+       int average= numOfChar/numOfWords;
+
+       System.out.println("Length of all the words is: "+ average);
+
+
+
+
+       }
+
+   }

4d640a65b1b02922be59c38bb7324d0e3b71b908
koko-99 <okousseila@gmail.com>
2022-10-13 16:15:07 -0600

    10/13/22 Permutation packge is completed

--- src/string_problems/Palindrome.java
+++ src/string_problems/Palindrome.java
@@ -11,8 +11,28 @@ public class Palindrome {
      */
 
     public static void main(String[] args) {
+        String original = "";
+        String reverse = "";
+        original= original.replace(" ", "");
 
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reverse += original.charAt(i);
+
+
+        }
+
+        boolean palindrome = true;
+        for (int i = 0; i < original.length(); i++) {
+            if (original.charAt(i) != reverse.charAt(i)) {
+                palindrome = false;
+            }
+        }
+        if (palindrome) {
+            System.out.println("Palindrome");
+
+        } else
+            System.out.println("Not a Palindrome");
     }
-    // Implement here
+
 
 }

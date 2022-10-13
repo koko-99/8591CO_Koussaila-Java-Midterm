ba9f97d14663647ea7427f5577f906544767c08a
koko-99 <okousseila@gmail.com>
2022-10-13 15:58:43 -0600

    Package FindLowestDifference is completed

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,5 +1,7 @@
 package math_problems;
 
+import java.util.*;
+
 public class FindLowestDifference {
 
     /** INSTRUCTIONS
@@ -13,8 +15,35 @@ public class FindLowestDifference {
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
 
-    }
+        Set<Integer> Sarray1 = new HashSet<Integer>();
+        Set<Integer> Sarray2 = new HashSet<Integer>();
+        for(int i=0;i<9;i++)
+        {
+            Sarray1.add(array1[i]);
+
+        }
+        for(int i=0;i<9;i++)
+        {
+            Sarray2.add(array2[i]);
+        }
+        int lowestNumber=array1[0];
+
+        for(int i=0;i<9;i++)
+        {
+            if(Sarray2.contains(array1[i])==false)
+            {
+                lowestNumber=array1[i]>lowestNumber?lowestNumber:array1[i];}}
+        for(int i=0;i<9;i++) {
+            if(Sarray1.contains(array2[i])==false) {
+                lowestNumber=array2[i]>lowestNumber?lowestNumber:array2[i];
+            } }
+        System.out.println(lowestNumber);
+        } }
 
 
 
-}
+
+
+
+
+

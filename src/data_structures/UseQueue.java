c2b10411c635ddcaed500458e0ec944c5f7608b1
koko-99 <okousseila@gmail.com>
2022-10-15 02:46:39 -0600

    10/14/22 UseQueue is completed

--- src/data_structures/UseQueue.java
+++ src/data_structures/UseQueue.java
@@ -1,5 +1,11 @@
 package data_structures;
 
+import databases.SharedStepsDatabase;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
     /** INSTRUCTIONS
@@ -11,8 +17,46 @@ public class UseQueue {
      */
 
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args)
+    {
+        //create a queue
+        Queue<Integer> q = new LinkedList<>();
+
+        // Adds elements {0, 1, 2, 3, 4} to
+        // the queue
+        int i=0;
+        while(i<7){
+            q.add(i);
+            i++;
+        }
+
+        /*// Display contents of the queue using foreach loop
+        System.out.println("Elements of queue : ");
+        for(int num:q)
+            System.out.print(num+" ");
+
+        System.out.print("");*/
+
+        // Demonstrate peek
+        int head = q.peek();
+        System.out.println("peek of queue: "+ head);
+
+        // To remove the head of queue.
+        int first = q.remove();
+        System.out.println("removed element: "+first);
+
+
+        // Display contents of the queue using while loop with iterator
+        System.out.println("Elements of queue: "+ q);
+        Iterator it=q.iterator();
+        while (it.hasNext())
+            System.out.print(it.next());
+        System.out.println(" "+" are the elements of the queue");
+        //poll element
+        int pollelement=q.poll();
+        System.out.println(" Queue's head: " + pollelement);
 
     }
-
 }

86e2dd3f4c9155c81099b8d1eaa0759ab1c50a8b
koko-99 <okousseila@gmail.com>
2022-10-15 23:41:40 -0600

    10/15/22 XmlReader partialy done

--- src/xml/XmlReader.java
+++ src/xml/XmlReader.java
@@ -76,6 +76,17 @@ public class XmlReader {
     public char convertIntToChar(String score) {
         char grade = ' ';
 
+        // we use a switch statement to convert the grades
+        switch (score) {
+            case "90":case "91": case "92": case "93":case "94":case "95":case "96":case "97":case "98":case "99":case "100":
+                return 'A';
+            case "80":case "81": case "82": case "83":case "84":case "85":case "86":case "87":case "88":case "89":
+                return 'B';
+            case "70":case "71": case "72": case "73":case "74":case "75":case "76":case "77":case "78":case "79":
+                return 'C';
+        }
+
+
         return grade;
     }
 
